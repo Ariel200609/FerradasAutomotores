0de891a7d2756c7c04dc1e5a81c18d3ea0458ae8
Ariel200609 <arielmontoya200609@gmail.com>
2025-07-05 19:47:41 -0300

    Primer commit

--- src/controllers/messageController.ts
+++
@@ -1,7 +0,0 @@
-import { sendContactMessage } from '../services/apiService';
-
-// Envía el mensaje del formulario de contacto al backend
-export const sendMessage = async ({ nombre, email, telefono, mensaje }: { nombre: string, email: string, telefono: string, mensaje: string }) => {
-  // Aquí podrías agregar validaciones adicionales
-  return sendContactMessage({ nombre, email, telefono, mensaje });
-}; 
