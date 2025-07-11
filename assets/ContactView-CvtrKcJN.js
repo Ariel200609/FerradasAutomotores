11be5679c7c3cd110bb34445d2bd36711ec6902d
Ariel <arielmontoya200609@gmail.com>
2025-07-11 14:25:38 -0300

    Updates

---
+++ assets/ContactView-CvtrKcJN.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BWUEAJOG.js";import o from"./ContactSection-BLFv6ggu.js";import"./createLucideIcon-CKD0JAns.js";const m=()=>t.jsx(o,{});export{m as default};
--- assets/createLucideIcon-CKD0JAns.js
+++ assets/createLucideIcon-CKD0JAns.js
@@ -1,4 +1,4 @@
-import{r as o}from"./index-CESQFXNX.js";/**
+import{r as o}from"./index-BWUEAJOG.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
