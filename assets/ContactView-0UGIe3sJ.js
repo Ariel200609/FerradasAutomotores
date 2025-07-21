805d7fb28f212d1d6a87b7e441e4e9a2971498ab
Ariel <arielmontoya200609@gmail.com>
2025-07-20 21:24:16 -0300

    Updates

---
+++ assets/ContactView-0UGIe3sJ.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DlInImKm.js";import o from"./ContactSection-Ibwhwl10.js";import"./createLucideIcon-BoDPvKbe.js";const m=()=>t.jsx(o,{});export{m as default};
--- assets/ContactView-CGAvj9ep.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-sQA2sCiI.js";import o from"./ContactSection-BaDQ5AWF.js";import"./createLucideIcon-CuVgHMoO.js";const m=()=>t.jsx(o,{});export{m as default};
