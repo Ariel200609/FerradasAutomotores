ffa4f2de420987b62cfe79057dc81594b2138b1e
Ariel <arielmontoya200609@gmail.com>
2025-07-30 01:31:26 -0300

    noseya

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  base: '/FerradasAutomotores/',
+  base: '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
